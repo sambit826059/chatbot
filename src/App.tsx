e9f91112af4d4064b03579c24902cb75c796dde4
sambit826059 <sambitmohanty1947@gmail.com>
2024-12-01 22:56:44 +0530

    simple ui for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { useState } from "react"
+import axios from "axios"
 
 function App() {
+  const [ response, setResponse ] = useState("Hi there! How can I be helpful to you ?");
+  const [ value, setValue ] = useState("");
+  
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
+    setValue(e.target.value);
+  }
+  const handleSubmit = async () => {
+    const response = await axios.post("http://localhost:3000/chatbot", {
+      question: value
+    });
+    setResponse(response.data)
+  };
 
   return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+    <div className="container">
+      <div>
+        <input type="text" 
+          value={value}
+          onChange={onChange}
+         />
+      </div>
+      <div>
+        <button onClick={handleSubmit}>Click me for answers!</button>
+      </div>
+      <div>
+        <p>Chatbot: {response}</p>
+      </div>
+    </div>
+    
+  );
 }
 
-export default App
+export default App;
